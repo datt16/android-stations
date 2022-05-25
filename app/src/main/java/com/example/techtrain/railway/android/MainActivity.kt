3fccd857bf293d885ae358562b86258eeb969632
datt16 <osatom.4639@gmail.com>
2022-05-25 23:15:30 +0900

    challenge station 9

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.techtrain.railway.android
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TextView>(R.id.text).text = getString(R.string.display_text)
+        val saveBtn = findViewById<Button>(R.id.save_button)
+        val textEditor = findViewById<EditText>(R.id.top_text_editor)
+        val savedTextView = findViewById<TextView>(R.id.saved_text_view)
+
+        saveBtn.setOnClickListener {
+            val newText = textEditor.text
+            savedTextView.text = newText
+        }
     }
 }
